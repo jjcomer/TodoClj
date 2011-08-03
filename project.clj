f13d0cd39f6a2a2d9a3d6ed64a38273a9b9f9563
Josh Comer <joshjcomer@gmail.com>
2011-08-02 21:15:05 -0300

    Added the file parser

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject TodoClj "1.0.0-SNAPSHOT"
   :description "A Todo manager written in Clojure"
   :url "http://jjcomer.github.com/TodoClj"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
--- src/TodoClj/core.clj
+++ src/TodoClj/core.clj
@@ -1,1 +1,17 @@
-(ns TodoClj.core)
+(ns TodoClj.core
+	(:require [clojure.contrib [duck-streams :as io]]))
+
+(def file-location "Todo.txt")
+
+(defn parse-line
+	"This function is to be used in the mapping of the file"
+	[[type & raw-desc]]
+	(let [description (apply str (interpose " " raw-desc))]
+		(if (= "T" type)
+			[:Todo description]
+			[:Done description])))
+
+(defn read-in-file
+	"Read in and convert the Todo file into an internal representation"
+	[file-location]
+	(map #(parse-line (str "[" % "]")) (io/read-lines file-location)))
